3723c2b1add80f9c7ae8a43a20e9d65c29ab60a1
eivancode <ivanalvarez78@aragon.unam.mx>
2022-08-17 12:30:16 -0500

    Eliminar imagen del servidor

--- delete.php
+++ delete.php
@@ -3,11 +3,16 @@ include_once 'model/conexion.php';
 
 if (isset($_POST['id'])) {
     $id = $_POST['id'];
+    $query = mysqli_query($conexion, "SELECT * FROM proyectos WHERE id = $id");
+    $getData = mysqli_fetch_assoc($query);
     
-    $query = "DELETE FROM proyectos WHERE id = $id";
-    $resultado = mysqli_query($conexion, $query);
-    echo('prueba');
-    if(!$resultado){
+    if($getData){
+        unlink('images/'.$getData['imagen']);
+        $query = mysqli_query($conexion, "DELETE FROM proyectos WHERE id = $id");
+        $delete = mysqli_fetch_assoc($query);
+    }
+
+    if(!$getData){
         die('Error'.mysqli_error($conexion));
     }
 }
