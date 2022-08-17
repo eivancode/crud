543f165a855bc1188c18cdca47c1045193ac71bf
eivancode <ivanalvarez78@aragon.unam.mx>
2022-08-17 12:35:11 -0500

    Subir y renombrar imagen

--- add.php
+++ add.php
@@ -4,10 +4,11 @@ include_once 'model/conexion.php';
 if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_FILES['image']['name'])) {
     $nombre = $_POST['name'];
     $imagen = $_FILES['image']['name'];
-    $img_tmp = $_FILES['image']['tmp_name'];
-    move_uploaded_file($_FILES['image']['tmp_name'], 'images/' .time(). $imagen);
+    //$img_tmp = $_FILES['image']['tmp_name'];
+    $renameImg = time().$imagen;
+    move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . $renameImg);
     $descripcion = $_POST['description'];
-    $query = "INSERT INTO proyectos(nombre, imagen, descripcion) VALUES ('$nombre', '$imagen', '$descripcion')";
+    $query = "INSERT INTO proyectos(nombre, imagen, descripcion) VALUES ('$nombre', '$renameImg', '$descripcion')";
     $resultado = mysqli_query($conexion, $query);
 
     if (!$resultado) {
