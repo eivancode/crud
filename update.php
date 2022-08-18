19a8dbf04dcdc11395f9ae8fc25a47509c9d0d30
eivancode <ivanalvarez78@aragon.unam.mx>
2022-08-18 12:19:45 -0500

    Se añaden condiciones para actualizar imagen

--- update.php
+++ update.php
@@ -1,16 +1,28 @@
 <?php
 include_once 'model/conexion.php';
 
-if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_FILES['image'])) {
-    $id = $_POST['id'];
+$id = $_POST['id'];
+$query = mysqli_query($conexion, "SELECT imagen FROM proyectos WHERE id = $id");
+$getData = mysqli_fetch_assoc($query);
+
+if (!empty($_POST['name']) && !empty($_POST['description']) && empty($_FILES['image']['name'])) {
+    $nombre = $_POST['name'];
+    $imagen = $getData['imagen'];
+    $descripcion = $_POST['description'];
+    $query = mysqli_query($conexion,"UPDATE proyectos SET nombre = '$nombre', imagen = '$imagen', descripcion = '$descripcion' WHERE id = '$id'");
+}
+
+if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_FILES['image']['name'])) {
     $nombre = $_POST['name'];
     $imagen = $_FILES['image']['name'];
+    $renameImg = time() . $imagen;
+    move_uploaded_file($_FILES['image']['tmp_name'], 'images/' . $renameImg);
     $descripcion = $_POST['description'];
+    $query = mysqli_query($conexion,"UPDATE proyectos SET nombre = '$nombre', imagen = '$renameImg', descripcion = '$descripcion' WHERE id = '$id'");
+    unlink('images/' . $getData['imagen']);
+}
 
-    //move_uploaded_file($_FILES['image']['tmp_name'], 'images/' .time(). $imagen);
-    $query = "UPDATE proyectos SET nombre = '$nombre', descripcion = '$descripcion', imagen = '$imagen' WHERE id = '$id'";
-    $resultado = mysqli_query($conexion, $query);
-    if (!$resultado) {
-        die('Error' . mysqli_error($conexion));
-    }
+
+if (!$resultado) {
+    die('Error' . mysqli_error($conexion));
 }
